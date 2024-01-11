809cf5cc574e08f4b4daa9baedae2fabf71a1a4b
Moshfegh Hamedani <programmingwithmosh@gmail.com>
2024-01-11 10:32:09 -0800

    Test-driven development

--- src/intro.js
+++ src/intro.js
@@ -10,3 +10,10 @@ export function fizzBuzz(n) {
   if (n % 5 === 0) return 'Buzz';
   return n.toString();
 }
+
+export function calculateAverage(numbers) {
+  if (numbers.length === 0) return NaN;
+
+  const sum = numbers.reduce((sum, current) => sum + current, 0);
+  return sum / numbers.length;
+}
--- tests/intro.test.js
+++ tests/intro.test.js
@@ -1,5 +1,5 @@
 import { describe, test, it, expect } from "vitest";
-import { fizzBuzz, max } from "../src/intro";
+import { calculateAverage, fizzBuzz, max } from "../src/intro";
 
 describe('max', () => {
   it('should return the first argument if it is greater', () => {
@@ -30,5 +30,23 @@ describe('fizzBuzz', () => {
 
   it('should return arg as a string if it is not divisible by 3 or 5', () => {
     expect(fizzBuzz(1)).toBe('1')
+  });
+})
+
+describe('calculateAverage', () => {
+  it('should return NaN if given an empty array', () => {
+    expect(calculateAverage([])).toBe(NaN);
+  });
+
+  it('should calculate the average of an array with a single element', () => {
+    expect(calculateAverage([1])).toBe(1);
+  });
+
+  it('should calculate the average of an array with two elements', () => {
+    expect(calculateAverage([1, 2])).toBe(1.5);
+  });
+
+  it('should calculate the average of an array with three elements', () => {
+    expect(calculateAverage([1, 2, 3])).toBe(2);
   });
 })
