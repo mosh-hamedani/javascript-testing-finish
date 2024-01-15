678e477f2604c72637023baba46924eaf306f38c
Moshfegh Hamedani <programmingwithmosh@gmail.com>
2024-01-15 12:00:47 -0800

    Positive and negative testing

--- tests/core.test.js
+++ tests/core.test.js
@@ -1,5 +1,5 @@
 import { it, expect, describe } from 'vitest';
-import { getCoupons } from '../src/core';
+import { calculateDiscount, getCoupons } from '../src/core';
 
 describe('getCoupons', () => {
   it('should return an array of coupons', () => {
@@ -27,3 +27,26 @@ describe('getCoupons', () => {
     });
   });
 });
+
+describe('calculateDiscount', () => {
+  it('should return discounted price if given valid code', () => {
+    expect(calculateDiscount(10, 'SAVE10')).toBe(9)
+    expect(calculateDiscount(10, 'SAVE20')).toBe(8)
+  });
+
+  it('should handle non-numeric price', () => {
+    expect(calculateDiscount('10', 'SAVE10')).toMatch(/invalid/i);
+  });
+
+  it('should handle negative price', () => {
+    expect(calculateDiscount(-10, 'SAVE10')).toMatch(/invalid/i);
+  });
+
+  it('should handle non-string discount code', () => {
+    expect(calculateDiscount(10, 10)).toMatch(/invalid/i);
+  });
+
+  it('should handle invalid discount code', () => {
+    expect(calculateDiscount(10, 'INVALID')).toBe(10);
+  })
+})
