61fa54864333058b863be225e6da7e3fd47f4d99
Moshfegh Hamedani <programmingwithmosh@gmail.com>
2024-01-16 08:55:21 -0800

    Boundary testing

--- tests/core.test.js
+++ tests/core.test.js
@@ -2,6 +2,7 @@ import { it, expect, describe } from 'vitest';
 import {
   calculateDiscount,
   getCoupons,
+  isPriceInRange,
   validateUserInput,
 } from '../src/core';
 
@@ -91,3 +92,19 @@ describe('validateUserInput', () => {
     expect(validateUserInput('', 0)).toMatch(/invalid age/i);
   });
 });
+
+describe('isPriceInRange', () => {
+  it('should return false when the price is outside the range', () => {
+    expect(isPriceInRange(-10, 0, 100)).toBe(false);
+    expect(isPriceInRange(200, 0, 100)).toBe(false);
+  })
+
+  it('should return true when the price is equal to the min or to the max', () => {
+    expect(isPriceInRange(0, 0, 100)).toBe(true);
+    expect(isPriceInRange(100, 0, 100)).toBe(true);
+  })
+
+  it('should return true when the price is within the range', () => {
+    expect(isPriceInRange(50, 0, 100)).toBe(true);
+  })
+})
