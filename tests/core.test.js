2d6a919ebfa8ede6191df560a5a72380c4139f75
Moshfegh Hamedani <programmingwithmosh@gmail.com>
2024-01-16 10:10:36 -0800

    Parameterized tests

--- tests/core.test.js
+++ tests/core.test.js
@@ -145,27 +145,14 @@ describe('canDrive', () => {
     expect(canDrive(20, 'FR')).toMatch(/invalid/i);
   });
 
-  it('should return false for underage in the US', () => {
-    expect(canDrive(15, 'US')).toBe(false);
-  });
-
-  it('should return true for min age in the US', () => {
-    expect(canDrive(16, 'US')).toBe(true);
-  });
-
-  it('should return true for eligible in the US', () => {
-    expect(canDrive(17, 'US')).toBe(true);
-  });
-
-  it('should return false for underage in the UK', () => {
-    expect(canDrive(16, 'UK')).toBe(false);
-  });
-
-  it('should return true for min age in the UK', () => {
-    expect(canDrive(17, 'UK')).toBe(true);
-  });
-
-  it('should return true for eligible in the UK', () => {
-    expect(canDrive(18, 'UK')).toBe(true);
-  });
+  it.each([
+    { age: 15, country: 'US', result: false },
+    { age: 16, country: 'US', result: true },
+    { age: 17, country: 'US', result: true },
+    { age: 16, country: 'UK', result: false },
+    { age: 17, country: 'UK', result: true },
+    { age: 18, country: 'UK', result: true },
+  ])('should return $result for $age, $country', ({ age, country, result }) => {
+    expect(canDrive(age, country)).toBe(result);
+  })
 })
