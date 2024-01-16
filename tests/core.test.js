f5922d7dbc71d34f83303612c662bbec8c60b595
Moshfegh Hamedani <programmingwithmosh@gmail.com>
2024-01-16 10:32:29 -0800

    Exercise: parameterized testing

--- tests/core.test.js
+++ tests/core.test.js
@@ -96,18 +96,14 @@ describe('validateUserInput', () => {
 });
 
 describe('isPriceInRange', () => {
-  it('should return false when the price is outside the range', () => {
-    expect(isPriceInRange(-10, 0, 100)).toBe(false);
-    expect(isPriceInRange(200, 0, 100)).toBe(false);
-  })
-
-  it('should return true when the price is equal to the min or to the max', () => {
-    expect(isPriceInRange(0, 0, 100)).toBe(true);
-    expect(isPriceInRange(100, 0, 100)).toBe(true);
-  })
-
-  it('should return true when the price is within the range', () => {
-    expect(isPriceInRange(50, 0, 100)).toBe(true);
+  it.each([
+    { scenario: 'price < min', price: -10, result: false },
+    { scenario: 'price = min', price: 0, result: true },
+    { scenario: 'price between min and max', price: 50, result: true },
+    { scenario: 'price = max', price: 100, result: true },
+    { scenario: 'price > max', price: 200, result: false },
+  ])('should return $result when $scenario', ({ price, result }) => {
+    expect(isPriceInRange(price, 0, 100)).toBe(result);
   })
 })
 
