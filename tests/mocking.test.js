e358ad2a4d80ccec592bc2a763a00bbe60974963
Moshfegh Hamedani <programmingwithmosh@gmail.com>
2024-01-18 11:10:34 -0800

    Partial mocking

--- tests/mocking.test.js
+++ tests/mocking.test.js
@@ -1,14 +1,22 @@
 import { vi, it, expect, describe } from 'vitest'
-import { getPriceInCurrency, getShippingInfo, renderPage, submitOrder } from '../src/mocking';
+import { getPriceInCurrency, getShippingInfo, renderPage, signUp, submitOrder } from '../src/mocking';
 import { getExchangeRate } from '../src/libs/currency';
 import { getShippingQuote } from '../src/libs/shipping';
 import { trackPageView } from '../src/libs/analytics';
 import { charge } from '../src/libs/payment';
+import { sendEmail } from '../src/libs/email';
 
 vi.mock('../src/libs/currency');
 vi.mock('../src/libs/shipping');
 vi.mock('../src/libs/analytics');
 vi.mock('../src/libs/payment');
+vi.mock('../src/libs/email', async (importOriginal) => {
+  const originalModule = await importOriginal();
+  return {
+    ...originalModule,
+    sendEmail: vi.fn()
+  }
+});
 
 describe('test suite', () => {
   it('test case', () => {
@@ -96,5 +104,30 @@ describe('submitOrder', () => {
     const result = await submitOrder(order, creditCard);
 
     expect(result).toEqual({ success: false, error: 'payment_error' })
+  })
+})
+
+describe('signUp', () => {
+  const email = 'name@domain.com';
+
+  it('should return false if email is not valid', async () => {
+    const result = await signUp('a');
+
+    expect(result).toBe(false);
+  })
+
+  it('should return true if email is valid', async () => {
+    const result = await signUp(email);
+
+    expect(result).toBe(true);
+  })
+
+  it('should send the welcome email if email is valid', async () => {
+    const result = await signUp(email);
+
+    expect(sendEmail).toHaveBeenCalled();
+    const args = vi.mocked(sendEmail).mock.calls[0];
+    expect(args[0]).toBe(email);
+    expect(args[1]).toMatch(/welcome/i);
   })
 })
